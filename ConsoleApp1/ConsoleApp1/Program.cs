12cc9c3c41e62eb4c847bcd2c92d0a1db0a647dd
ids ids <Idans1@outlook.co.il>
2019-12-08 20:45:32 +0200

    test

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             string text = "some text";
+             int num = text.IndexOf("text");
+             Console.WriteLine(num);
+             Console.WriteLine(2 / 2 + " " + 5 / 2 + " " + 5.0 / 2);
+             num++;
+             Console.WriteLine(Math.Pow(10,2));
+             Console.ReadLine();
+
+            //---------------------------------------------------------
+           
+            Console.Write("Enter name here: ");
+            string name = Console.ReadLine();
+            Console.Write("Hello " +name+ " enter your age: ");
+            int age = Convert.ToInt32((Console.ReadLine()));
+            Console.WriteLine("Hello " + name + " were born in " + (2019-age));
+           
+            //---------------------------------------------------------
+
+            Console.Write("Enter num 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter num 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + (num1 + num2));
+            */
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
